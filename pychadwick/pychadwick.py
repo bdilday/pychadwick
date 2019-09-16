527e5750cc397acca3a9cccda989c34a5b75239c
Ben Dilday <ben.dilday.phd@gmail.com>
2019-09-15 22:17:55 -0400

    initial dictionary event parsing support

--- pychadwick/pychadwick.py
+++ pychadwick/pychadwick.py
@@ -16,20 +16,41 @@ from .utils import CWEventFieldStruct
 
 
 class Chadwick:
+    FIELDS_COUNT = 96
+    EXT_FIELDS_COUNT = 63
+
     def __init__(self, library_path="libchadwick2.so", *args, **kwargs):
         self._dll = None
         self.library_path = library_path
         self._load_shared_library(library_path)
 
     @property
+    def cwevent_headers(self):
+        return [p.header for p in self.cwevent_field_data]
+
+    @property
+    def cwevent_ext_headers(self):
+        return [p.header for p in self.cwevent_ext_field_data]
+
+    @property
     def cwevent_field_data(self):
-        p = CWEventFieldStruct * 96
+        p = CWEventFieldStruct * self.FIELDS_COUNT
         return p.in_dll(self.libchadwick, "cwevent_field_data")
 
     @property
     def cwevent_fields(self):
-        p = c_int * 96
+        p = c_int * self.FIELDS_COUNT
         return p.in_dll(self.libchadwick, "fields")
+
+    @property
+    def cwevent_ext_field_data(self):
+        p = CWEventFieldStruct * self.EXT_FIELDS_COUNT
+        return p.in_dll(self.libchadwick, "cwevent_ext_field_data")
+
+    @property
+    def cwevent_ext_fields(self):
+        p = c_int * self.EXT_FIELDS_COUNT
+        return p.in_dll(self.libchadwick, "ext_fields")
 
     @property
     def libchadwick(self):
@@ -68,13 +89,38 @@ class Chadwick:
         cw_game_read = self.libchadwick.cw_game_read
         cw_game_read.restype = POINTER(CWGame)
         cw_game_read.argtypes = (ctypes.c_void_p,)
-        file_handle = self._file_open(file_path)
+        file_handle = self.fopen(file_path)
         while not self.feof(file_handle):
             try:
                 yield cw_game_read(file_handle)
             except:
                 self.fclose(file_handle)
                 return
+
+    @property
+    def active_headers(self):
+        return [
+            h.decode()
+            for i, h in enumerate(self.cwevent_headers)
+            if self.cwevent_fields[i] == 1
+        ] + [
+            h.decode()
+            for i, h in enumerate(self.cwevent_ext_headers)
+            if self.cwevent_ext_fields[i] == 1
+        ]
+
+    def dicticize_event_string(self, event_bytes, headers=None):
+        if headers is None:
+            headers = self.active_headers
+
+        return dict(zip(headers, self.listicize_event_string(event_bytes)))
+
+    @staticmethod
+    def listicize_event_string(event_bytes):
+        return [
+            event_item.replace(r'"', "")
+            for event_item in event_bytes.decode().split(",")
+        ]
 
     def process_game(self, game_ptr):
         cwevent_process_game_record = self.libchadwick.cwevent_process_game_record
@@ -92,11 +138,11 @@ class Chadwick:
         s = create_string_buffer(b" ", 4096)
         while gi.contents.event:
             cwevent_process_game_record(gi, rp1, rp2, s)
-            yield s.value
+            self.cw_gameiter_next(gi)
+            yield self.dicticize_event_string(s.value)
 
     def cw_gameiter_create(self, game_ptr):
         func = self.libchadwick.cw_gameiter_create
         func.restype = POINTER(CWGameIterator)
         func.argtypes = (POINTER(CWGame),)
         return func(game_ptr)
-
