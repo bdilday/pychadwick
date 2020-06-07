bc2c61389aa7237abd28d1adb816999106d0979b
Ben Dilday <bdilday@users.noreply.github.com>
2020-06-07 14:43:52 -0400

    bugfix - seg fault if `id` not first record (#3)
    * applies cw_find_first_game
    
    - this avoids segmentation fault when the first line of the file is a com
      e.g., 1991BAL.EVA
    
    * updates test
    
    * adds requests to requirements-dev
    
    * updates test to additional cases

--- tests/pychadwick/test_pychadwick.py
+++ tests/pychadwick/test_pychadwick.py
@@ -1,15 +1,29 @@
 import pytest
 from pychadwick.chadwick import Chadwick
+import tempfile
+import requests
+
+def get_event_path(url):
+    file_path = tempfile.gettempdir() + "/tmp.EVA"
+    with open(file_path, "w") as fh:
+        fh.write(requests.get(url).text)
+    return file_path
 
 
-@pytest.fixture
-def lib_path():
-    return (
-        "/home/bdilday/.venvs/pychadwick/lib/python3.7/"
-        "site-packages/pychadwick-0.1.0-py3.7-linux-x86_64.egg/"
-        "pychadwick/build/cwevent/libcwevent.so"
-    )
+def test_chadwick():
+    _ = Chadwick()
 
 
-def test_chadwick(lib_path):
-    chadwick = Chadwick(lib_path)
+def test_load_games():
+    chadwick = Chadwick()
+
+
+    for team_event in ["1982OAK", "1991BAL"]:
+        event_path = get_event_path(
+        f"https://raw.githubusercontent.com/chadwickbureau/retrosheet/master/event/regular/{team_event}.EVA"
+        )
+        games = chadwick.games(event_path)
+        game = next(games)
+        game_it = chadwick.process_game(game)
+        record = next(game_it)
+        assert "GAME_ID" in record.keys()
