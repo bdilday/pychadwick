66b1d3b44e4fcc31d8d925b58076f1f5c3ca26ff
Ben Dilday <ben.dilday.phd@gmail.com>
2020-09-04 22:02:41 -0400

    bumps version to 0.3.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="pychadwick",
-    version="0.3.2",
+    version="0.3.3",
     author="Ben Dilday",
     author_email="ben.dilday.phd@gmail.com",
     description="Python bindings to the Chadwick library",
