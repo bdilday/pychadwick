08e302e97153c58646ef9ea9ca96a4758ff6e735
Ben Dilday <bdilday@users.noreply.github.com>
2021-02-06 08:32:43 -0500

    adds platform check in library load (#27)
    * adds platform check in library load
    
    * bumps version
    
    * bumps version

--- pychadwick/libutils/cwlib.py
+++ pychadwick/libutils/cwlib.py
@@ -1,13 +1,14 @@
 import ctypes
 import pathlib
+import sys
 
-
+FILE_EXTENSION = {"darwin": "dylib", "linux": "so", "windows": "dll"}
 class _ChadwickLibrary:
     library_path = (
         pathlib.Path(__file__).absolute().parent.parent
         / "lib"
         / "cwevent"
-        / "libcwevent.so"
+        / f"libcwevent.{FILE_EXTENSION.get(sys.platform)}"
     )
 
     def __init__(self):
